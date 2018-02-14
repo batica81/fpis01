2c89691bc8ecb4dff5ea70eab00b513aeb1cc67c
voja <batica@gmail.com>
2018-02-14 20:40:48 +0100

    mock data, db checkup

--- src/com/fpis/test/dbbroker/DBbroker.java
+++ src/com/fpis/test/dbbroker/DBbroker.java
@@ -5,7 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
 import java.util.Collection;
 import java.util.List;
 
